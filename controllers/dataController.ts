a76312a266817e7660675f113b421b859e7dea2d
Darlon Gomes <gomesdarlon@icloud.com>
2022-08-31 20:26:39 -0300

    finish

--- controllers/dataController.ts
+++ controllers/dataController.ts
@@ -1,15 +1,46 @@
 import { Request, Response } from "express";
 import "../dbstrategy/config/config"
 import { starfighter } from "../repositories/battleRepository";
-import { getUserStar } from "../services/github";
+import { checkIfUserExist, getUserStar } from "../services/services";
 
 export async function starBattle (req: Request, res: Response){
     const {firstUser, secondUser} = req.body;
    try {
+    const checkFirstUser = checkIfUserExist(firstUser);
+    const checkSecondUser = checkIfUserExist(secondUser);
+
+    if(!checkFirstUser) await starfighter.createUser(firstUser);
+    if(!checkSecondUser) await starfighter.createUser(secondUser);
+
     const firstValue = await getUserStar(firstUser);
     const secondValue = await getUserStar(secondUser);
-    
-    return res.status(200).send({firstValue, secondValue})
+    if(firstValue> secondValue){
+        await starfighter.updateUser(firstUser, "wins");
+        await starfighter.updateUser(secondUser, "losses");
+        return res.status(200).send({
+            "winner": firstUser, 
+          "loser": secondUser, 
+            "draw": false 
+        })
+    }
+    if(firstValue < secondValue){
+        await starfighter.updateUser(secondUser, "wins");
+        await starfighter.updateUser(firstUser, "losses");
+        return res.status(200).send({
+            "winner": secondUser, 
+          "loser": firstUser, 
+            "draw": false 
+        })
+    }
+    if(firstValue === secondValue){
+        await starfighter.updateUser(firstUser,"draws");
+        await starfighter.updateUser(secondUser, "draws");
+        return res.status(200).send({
+            "winner": null, 
+          "loser": null, 
+            "draw": true 
+        })
+    }
    } catch (error) {
     return res.status(500).send(error)
    }
--- services/services.ts
+++ services/services.ts
@@ -1,9 +1,14 @@
 
 import axios from "axios";
+import { starfighter } from "../repositories/battleRepository";
 
 export async function getUserStar (user:string){
     const { data: userReposInfo } = await axios.get(`https://api.github.com/users/${user}/repos`);
     const userStargazersCount: number = userReposInfo.reduce((sum: number, repo: any) => (sum + repo.stargazers_count), 0);
-    console.log(userReposInfo)
     return userStargazersCount
+}
+
+export async function checkIfUserExist (user:string){
+    const { rows: [userInfo]} = await starfighter.checkUser(user);
+    return userInfo
 }
